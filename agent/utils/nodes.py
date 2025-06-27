8d4341c9fd15203473fb093e4239c4fad2a973ff
feather0611 <feather0611@icloud.com>
2025-06-27 22:26:38 +0800

    Implement geocode_node

---
+++ agent/utils/nodes.py
@@ -0,0 +1,19 @@
+from .state import AppState
+from .tools.weather_tools import get_geocode_of_location
+
+def geocode_node(state: AppState) -> dict:
+    print("--- Run node: geocode_node ---")
+
+    user_input = state["user_input"]
+    api_key = state["api_keys"].get("google")
+
+    if not api_key:
+        return {"error_message": "Google Maps API Key is missing"}
+
+    results = get_geocode_of_location(user_input, api_key)
+
+    if results is None:
+        return {"error_message": "geocoding tool request failed, please check API key or network connection."}
+
+    print(f"    -> Geocoding tool found {len(results)} of possible locations")
+    return {"choices": results}
